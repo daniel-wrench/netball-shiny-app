8baea1af5753acd2a85f64b8f43bfe0b4ae63c30
Daniel Wrench <daniel.wrench@universitiesnz.ac.nz>
2021-07-26 14:57:31 +1200

    Adding (unreactive) plots

--- app.R
+++ app.R
@@ -1,6 +1,13 @@
 #
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
+# DATA471: Assignment 2 Part 2
+# Netball data Shiny app
+#
+# NEXT STEPS:
+# Insert plots into Shiny app 
+# Add reactivity
+#
+# ISSUES:
+#
 #
 # Find out more about building applications with Shiny here:
 #
@@ -16,11 +23,28 @@ netball_data <- read_csv("ANZ_Premiership_2017_2020.csv")
 head(netball_data)
 nrow(netball_data)
 
+# Bar plot of totals per team
+team_summary <- netball_data %>% 
+    group_by(Team) %>%
+    summarise("Wins" = sum(W), "Bonus points" = sum(BP))
+
+team_summary_long <- team_summary %>%
+    pivot_longer(cols = c("Wins", "Bonus points"), names_to = "Statistic", values_to = "Total")
+
+ggplot(data=team_summary_long, aes(x = Team, y = Total, fill = Statistic)) + geom_col(position = "dodge")
+
+# Line plot of points per team over time
+ggplot(data=netball_data, aes(x = Year, y = Pts, colour = Team)) + geom_line()
+
+# Scatter plot of goals for vs. goals against
+ggplot(data = netball_data, aes(x = GA, y = GF, colour = Team)) + geom_point()
+
+
 # Define UI for application that draws a histogram
 ui <- fluidPage(
 
     # Application title
-    titlePanel("Old Faithful Geyser Data"),
+    titlePanel("ANZ Premiership Data"),
 
     # Sidebar with a slider input for number of bins 
     sidebarLayout(
@@ -34,7 +58,7 @@ ui <- fluidPage(
 
         # Show a plot of the generated distribution
         mainPanel(
-           plotOutput("distPlot")
+           plotOutput("scorePlot")
         )
     )
 )
@@ -49,6 +73,10 @@ server <- function(input, output) {
 
         # draw the histogram with the specified number of bins
         hist(x, breaks = bins, col = 'darkgray', border = 'white')
+    })
+    
+    output$scorePlot <- renderPlot({
+        
     })
 }
 
